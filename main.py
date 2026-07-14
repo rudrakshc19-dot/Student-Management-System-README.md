8bb6ef6472fdbe4079e8454ebe2ddcc92fc74393
rudrakshc19-dot <rudrakshc19@gmail.com>
2026-07-14 15:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,3 +68,47 @@ while True:
         print("Invalid Choice!")
 
 conn.close()
+# menu
+print("\n===== Student Management System =====")
+print("1. Add Student")
+print("2. View Students")
+print("3. Search Student")
+print("4. Update Student")
+print("5. Delete Student")
+print("6. Exit")
+ #search students 
+elif choice == "3":
+    name = input("Enter student name: ")
+    cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+    student = cursor.fetchall()
+
+    if student:
+        for s in student:
+            print(s)
+    else:
+        print("Student not found!")
+#update student 
+elif choice == "4":
+    student_id = input("Enter Student ID: ")
+    new_name = input("Enter New Name: ")
+    new_age = input("Enter New Age: ")
+    new_course = input("Enter New Course: ")
+
+    cursor.execute(
+        "UPDATE students SET name=?, age=?, course=? WHERE id=?",
+        (new_name, new_age, new_course, student_id)
+    )
+    conn.commit()
+    print("Student Updated Successfully!")
+# del stu
+elif choice == "5":
+    student_id = input("Enter Student ID: ")
+
+    cursor.execute("DELETE FROM students WHERE id=?", (student_id,))
+    conn.commit()
+
+    print("Student Deleted Successfully!")
+#breck
+elif choice == "6":
+    print("Thank You!")
+    break
